55caceaa320848113fd3f448327f5d3d5ece5c87
Kudzai Nyandoro <kudzain@gmail.com>
2018-08-23 05:01:56 -0700

    Add first templates.

--- first_app/views.py
+++ first_app/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    my_dict = {'insert_me':"Hello I am from first app views.py"}
+    return render(request,'first_app/index.html',context=my_dict)
 
