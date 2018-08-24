c84a01da21fdfac02d55e9895e07fa582b7b60a8
Kudzai Nyandoro <kudzain@gmail.com>
2018-08-24 05:11:39 -0700

    Add models and migrations.

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264,unique=True)
+
+    def __strr__(self):
+        return self.top_name
+
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic)
+    name = models.CharField(max_length=264,unique=True)
+    url = models.URLField(unique=True)
+
+    def __strr__(self):
+        return self.name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage)
+    date = models.DateField()
+
+    def __strr__(self):
+        return str(self.date)
